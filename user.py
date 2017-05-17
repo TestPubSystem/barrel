c1906d0a13f33ff6e3806a3254099ecd2ee3f603
Tishka17 <tishka17@mail.ru>
2017-05-17 21:47:18 +0300

    base classes

---
+++ user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import enum
+
+
+@enum.unique
+class Role(enum.Enum):
+    manager = "manager"
+    tester = "tester"
+    test_designer = "tets_designer"
+
+
+class UserAuth:
+    user_id = None
+    salt = None
+    hash = None
+
+
+class User:
+    roles = None  # type: list[Role]
+    login = None
+    name = None
+    email = None
+    avatar = None
+    registration_date = None  # type: datetime.datetime
+    blocked = False  # type: bool
+    confirmed = False  # type: bool
