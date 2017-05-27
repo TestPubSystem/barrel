78337e3328a3d5a870260afaec4d56a641e8967e
Tishka17 <tishka17@mail.ru>
2017-05-27 17:06:10 +0300

    import cors in debug

--- app.py
+++ app.py
@@ -7,15 +7,21 @@ from api.test import test_blueprint
 from api.test_suite import test_suite_blueprint
 from api.tag import tag_blueprint
 from db_json_encoder import CustomJSONEncoder
+from flask_cors import CORS, cross_origin
+
+DEBUG = True  # FIXME remove in production
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.json_encoder = CustomJSONEncoder
+
 db.init_app(app)
 db.app = app
-
 db.create_all(app=app)
+
+if DEBUG:
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(test_blueprint, url_prefix="/api/v1/tests")
 app.register_blueprint(test_suite_blueprint, url_prefix="/api/v1/testsuites")
