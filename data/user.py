697ded71c722072c38be3962da54226bea2c26ba
Tishka17 <tishka17@mail.ru>
2017-05-31 18:54:43 +0300

    user tables

--- data/user.py
+++ data/user.py
@@ -3,6 +3,10 @@
 
 import datetime
 import enum
+import random
+import string
+
+from data import db
 
 
 @enum.unique
@@ -10,20 +14,26 @@ class Role(enum.Enum):
     manager = "manager"
     tester = "tester"
     test_designer = "test_designer"
+    admin = "admin"
 
 
-class UserAuth:
-    user_id = None
-    salt = None
-    hash = None
+def gen_salt():
+    return "".join(random.choice(string.ascii_letters) for i in range(63))
 
 
-class User:
-    roles = None  # type: list[Role]
-    login = None
-    name = None
-    email = None
-    avatar = None
-    registration_date = None  # type: datetime.datetime
-    blocked = False  # type: bool
-    confirmed = False  # type: bool
+class UserAuth(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    salt = db.Column(db.String(64), nullable=False, default=gen_salt)
+    password_sha256 = db.Column(db.String(64), nullable=True)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    roles = db.Column(db.Enum(Role))
+    login = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    # avatar =
+    registration_date = db.Column(db.DateTime, default=db.func.now())
+    blocked = db.Column(db.Boolean, default=False)
+    confirmed = db.Column(db.Boolean, default=False)
