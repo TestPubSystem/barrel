8540b7435a12d42c1d5a3a757ba30bf1ba0ea3f7
Tishka17 <tishka17@mail.ru>
2017-06-07 19:38:44 +0300

    author and responsible in suite runs

--- api/suite_run.py
+++ api/suite_run.py
@@ -3,31 +3,38 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
+from flask_jwt import jwt_required, current_identity
 
 from data import db
 from data import suite_run
 from data.test_suite import TestSuite
+from data.user import User
 
 suite_run_blueprint = Blueprint("suite_run", __name__)
 
 
 @suite_run_blueprint.route("/")
+@jwt_required()
 def get_suite_runs():
     runs = suite_run.SuiteRun.query.all()
     return jsonify(data=runs)
 
 
 @suite_run_blueprint.route("/<int:run_id>")
+@jwt_required()
 def get_suite_run(run_id):
     runs = suite_run.SuiteRun.query.get(run_id)
     return jsonify(data=runs)
 
 
 @suite_run_blueprint.route("/", methods=["POST"])
+@jwt_required()
 def create_suite_run():
     data = request.get_json(force=True)
     suite = TestSuite.query.get(data["test_suite_id"])
-    run = suite_run.create_from_test_suite(suite)
+    responsible_id = data.get("responsible", {}).get("id")
+    responsible = User.query.get(responsible_id) if responsible_id else None
+    run = suite_run.create_from_test_suite(suite, current_identity, responsible)
     db.session.add(run)
     db.session.commit()
     return jsonify(data=run)
--- data/suite_run.py
+++ data/suite_run.py
@@ -4,16 +4,22 @@
 from data import db
 from data.test_suite import TestSuite
 from data.test_run import TestRun
+from data.user import User
 import data.test_run
+
 
 class SuiteRun(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     test_suite_id = db.Column(db.Integer, db.ForeignKey('test_suite.id'))
     start_date = db.Column(db.DateTime, default=db.func.now())
     finish_date = db.Column(db.DateTime)
-    test_runs = db.relationship("TestRun", cascade="all")
+    test_runs = db.relationship("TestRun", cascade="all")  # type: list[TestRun]
 
-    # author_id = None
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    author = db.relationship("User", backref="suite_runs", foreign_keys=[author_id])  # type: User
+
+    responsible_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    responsible = db.relationship("User", backref="responsible_suite_runs", foreign_keys=[responsible_id])  # type: User
 
     def to_map(self):
         return {
@@ -22,11 +28,15 @@ class SuiteRun(db.Model):
             "start_date": self.start_date,
             "finish_date": self.finish_date,
             "test_runs": self.test_runs,
+            "author": self.author,
+            "responsible": self.responsible
         }
 
 
-def create_from_test_suite(test_suite: TestSuite):
+def create_from_test_suite(test_suite: TestSuite, author: User, responsible: User):
     run = SuiteRun()
+    run.author = author
+    run.responsible = responsible
     run.test_suite_id = test_suite.id
     for test in test_suite.tests:
         res = data.test_run.create_from_test(test)
