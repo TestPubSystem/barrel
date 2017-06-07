5c7dd1c2bb5924ce2353d17dc13e388c53264cbd
Tishka17 <tishka17@mail.ru>
2017-06-07 21:37:47 +0300

    fix step result auth

--- api/step_result.py
+++ api/step_result.py
@@ -5,15 +5,18 @@ from data import db
 from data import result
 
 from flask import request, Blueprint, jsonify
+from flask_jwt import jwt_required
 
 step_result_blueprint = Blueprint("step_result", __name__)
 
 
 @step_result_blueprint.route("/<int:result_id>", methods=["PUT"])
+@jwt_required()
 def update_step_result(result_id):
     res = result.StepResult.query.get(result_id)
     if not res:
         return "Step result not found", 404
     data = request.get_json(force=True)
     res.update_from_map(data)
+    db.session.commit()
     return jsonify(data=res)
