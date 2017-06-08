abead10898775729dfbf57f12a4c180a8a26b913
Tishka17 <tishka17@mail.ru>
2017-06-08 08:11:53 +0300

    project api

--- api/project.py
+++ api/project.py
@@ -1,2 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask_jwt import jwt_required
+
+from data import project
+from data import db
+
+project_blueprint = Blueprint("project", __name__)
+
+
+@project_blueprint.route("/", methods=["GET"])
+@jwt_required()
+def get_projects(offset=0, limit=20):
+    res = project.Project.query.offset(offset).limit(limit).all()
+    return jsonify(data=res)
+
+
+@project_blueprint.route("/", methods=["POST"])
+@jwt_required()
+def create_project():
+    res = project.Project()
+    res.update_from_map()
+    db.session.add(res)
+    db.session.commit()
+    return jsonify(data=res)
+
+
+@project_blueprint.route("/<int:project_id>", methods=["GET"])
+@jwt_required()
+def get_project(project_id):
+    proj = project.Project.query.get(project_id)  # type: project.Project
+    if not proj:
+        return "Project not found", 404
+    return jsonify(data=proj)
+
+
+@project_blueprint.route("/<int:project_id>", methods=["PATCH"])
+@jwt_required()
+def patch_project(project_id):
+    proj = project.Project.query.get(project_id)  # type: project.Project
+    if not proj:
+        return "Project not found", 404
+    data = request.get_json(force=True)
+    proj.update_from_map(data)
+    db.session.commit()
+    return jsonify(data=proj)
--- data/project.py
+++ data/project.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from data import db
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    desc = db.Column(db.String(140))
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    author = db.relationship("User", backref="test_runs", foreign_keys=[author_id])  # type: User
+    creation_date = db.Column(db.DateTime, default=db.func.now())
+
+    def to_map(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "desc": self.desc,
+            "author": self.author,
+            "creation_date": self.creation_date,
+        }
+
+    def update_from_map(self, data):
+        self.name = data.get("name", self.name)
+        self.desc = data.get("desc", self.desc)
